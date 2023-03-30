8edae118d868e25fcb13d428a7c84818f6832681
Ricardo Viana <richardluizv@gmail.com>
2023-03-30 20:20:20 -0300

    add endpoint get

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -3,11 +3,27 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Ricardolv/vacancies/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "get",
-	})
+
+	// Get the user in ctx
+	id := ctx.Query("id")
+	if len(id) == 0 {
+		sendError(ctx, http.StatusBadRequest,
+			errParamIsRequest("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	// Find the opening
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNoContent, "")
+		return
+	}
+
+	sendSuccessOK(ctx, "show-opening", opening)
 }
