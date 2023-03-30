7a8d6a92028c76a2137b1268b83994841d030d19
Ricardo Viana <richardluizv@gmail.com>
2023-03-30 20:19:50 -0300

    add endpoint delete

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -1,13 +1,36 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Ricardolv/vacancies/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "get",
-	})
+
+	id := ctx.Query("id")
+	if len(id) == 0 {
+		sendError(ctx, http.StatusBadRequest,
+			errParamIsRequest("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	// Find the opening
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNoContent, "")
+		return
+	}
+
+	// Delete the opening
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError,
+			fmt.Sprintf("error deleting opening whit id: %s", id))
+		return
+	}
+
+	sendSuccessOK(ctx, "deleted-opening", opening)
 }
