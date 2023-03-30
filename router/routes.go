eeac192e860c8e2c6d450b5c45e2e49145bc18ce
Ricardo Viana <richardluizv@gmail.com>
2023-03-29 21:10:43 -0300

    add routes

--- router/routes.go
+++ router/routes.go
@@ -1,16 +1,38 @@
 package router 
 
 import ( 
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func InitializeRoutes(router *gin.Engine) {
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	v1 := router.Group("/api/v1") 
+	{
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "get",
+			})
 		})
-	})
 
+        v1.POST("/opening", func(ctx *gin.Context) {
+            ctx.JSON(http.StatusOK, gin.H{
+                "message": "post",
+            })
+        })
 
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+                    "message": "delete",
+                })
+        })
+
+		v1.PUT("/opening", func(ctx *gin.Context) {
+            ctx.JSON(http.StatusOK, gin.H{
+                "message": "put",
+            })
+        })
+		
+	}
 } 
