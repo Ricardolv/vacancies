770664dd964fe6fc80e7107ad1d5371616ef3ed1
Ricardo Viana <richardluizv@gmail.com>
2023-03-30 09:59:02 -0300

    add log structure

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/Ricardolv/vacancies/router"
+import (
+	"github.com/Ricardolv/vacancies/config"
+	"github.com/Ricardolv/vacancies/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// initialize Configs
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 
 	// initialize Router
 	router.Initialize()
