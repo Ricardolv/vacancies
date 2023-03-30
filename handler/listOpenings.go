ed676a18676d29291efa48a425ee3b29f979732a
Ricardo Viana <richardluizv@gmail.com>
2023-03-30 20:20:03 -0300

    add endpoint list

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -3,11 +3,18 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Ricardolv/vacancies/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "get",
-	})
+
+	openings := []schemas.Opening{}
+	// Find the opening
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "")
+		return
+	}
+
+	sendSuccessOK(ctx, "list-opening", openings)
 }
