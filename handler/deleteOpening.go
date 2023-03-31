c3e6cd9492a1c39fc22a39db371c754fbf9ce6d9
Ricardo Viana <richardluizv@gmail.com>
2023-03-30 21:29:59 -0300

    update swagger

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -8,6 +8,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /api/v1
+
+// @Summary Delete opening
+// @Description Delete a new job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param id query string true "Opening identification"
+// @Success 204
+// @Failure 400 {object} ErrorResponse
+// @Failure 204 {object} ErrorResponse
+// @Router /openings [delete]
 func DeleteOpeningHandler(ctx *gin.Context) {
 
 	id := ctx.Query("id")
--- handler/response.go
+++ handler/response.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/Ricardolv/vacancies/schemas"
 	"github.com/gin-gonic/gin"
 )
 
@@ -27,4 +28,24 @@ func sendSuccessCreated(ctx *gin.Context, id int) {
 	ctx.Header("Content-Type", "application/json")
 	ctx.Header("location", fmt.Sprintf("localhost:8080/api/v1/openings/%v", id))
 	ctx.AbortWithStatus(http.StatusCreated)
+}
+
+type ErrorResponse struct {
+	Message   string `json:"message"`
+	ErrorCode string `json:"errorCode"`
+}
+
+type ShowOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+
+type ListOpeningsResponse struct {
+	Message string                    `json:"message"`
+	Data    []schemas.OpeningResponse `json:"data"`
+}
+
+type UpdateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
 }
